5399f934e3da1f43ef8974f5ed5f256eab7f0e67
MoqeetUlHassan <moqeet1998@gmail.com>
2024-08-11 05:24:24 +0500

    crud apis

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SearchItem from './SearchItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState, useEffect } from 'react'
+import apiRequest from './apiRequest';
 
 function App() {
 
@@ -51,12 +52,21 @@ function App() {
   ])
 
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
 
+    const postOptions ={
+      method : 'POST',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL,postOptions );
+    if(result) setFetchError(result);
   }
 
   const handleSubmit = (e) => {
@@ -66,14 +76,35 @@ function App() {
     setNewItem('');
   }
 
-  const handleCheck = (id) => {
+  const handleCheck =  async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
+    
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions ={
+      method: 'PATCH',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({checked: myItem[0].checked})
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl,updateOptions);
+    if(result)setFetchError(result);
+
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions ={
+      method:'DELETE'
+    };
+
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl,deleteOptions);
+    if(result)setFetchError(result);
   }
 
 
